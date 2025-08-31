8c8fb55b7dc42be37b2444a905c2655525c9274e
SerKo <serko.dev@gmail.com>
2025-09-01 03:10:44 +0800

    feat: verify and preview recipients

--- src/index.tsx
+++ src/index.tsx
@@ -14,28 +14,29 @@ interface Recipient {
   email: string;
 }
 
-const data = readFileSync("./recipients.yaml", "utf-8");
-const recipientsMap = yaml.parse(data) as Record<string, string>;
-const recipients: Recipient[] = Object.entries(recipientsMap).map(
-  ([email, name]) => ({ email, name })
-);
-
+const recipients = resolveRecipients("./recipients.yaml");
 const senderEmail = process.env.SENDER_EMAIL || "registration@python.tw";
 const senderName = process.env.SENDER_NAME || "PyCon Taiwan";
 
+// show send info and preview
 consola.box(`\`PyCon Taiwan 2025\` Attendee Info Email Sender
   
 Sender name: \`${senderName}\`
 Sender email: \`${senderEmail}\`
-Email recipients: \`${recipients.length}\` (total \`${recipients.length * 2}\` emails)`);
+Email recipients: \`${recipients.length}\` (total \`${
+  recipients.length * 2
+}\` emails)
+Recipients preview:
+${getPreviewRecipients(recipients)}`);
 
-const confirm = await consola.prompt(`Confirm to send?`, {
-  type: "confirm",
-  initial: false,
-  cancel: "null",
-});
-
-if (!confirm) {
+// confirm to send
+if (
+  !(await consola.prompt(`Confirm to send?`, {
+    type: "confirm",
+    initial: false,
+    cancel: "null",
+  }))
+) {
   process.exit(0);
 }
 
@@ -45,6 +46,7 @@ const sender = createSender({
   senderName,
 });
 
+// start send emails
 for (const recipient of recipients) {
   await sender.send(
     "zh",
@@ -60,5 +62,33 @@ for (const recipient of recipients) {
     recipient.name,
     subjectEng,
     await render(<PyConWelcomeEmailEng recipient={recipient.name} />)
+  );
+}
+
+// Utils
+
+function resolveRecipients(path: string): Recipient[] {
+  const data = readFileSync(path, "utf-8");
+  const recipientsMap = yaml.parse(data) as Record<string, string>;
+  if (typeof recipientsMap !== "object")
+    throw new Error("Invalid recipients format");
+
+  const recipients: Recipient[] = [];
+
+  for (const [email, name] of Object.entries(recipientsMap)) {
+    recipients.push({ email, name });
+  }
+
+  if (recipients.length === 0) throw new Error("No recipients found");
+
+  return recipients;
+}
+
+function getPreviewRecipients(recipients: Recipient[], count = 3): string {
+  return (
+    recipients
+      .slice(0, count)
+      .map((r) => `  ${r.name} <${r.email}>`)
+      .join("\n") + (recipients.length > count ? "\n..." : "")
   );
 }
